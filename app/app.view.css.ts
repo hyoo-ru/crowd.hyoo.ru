490f58800502bd668a72a1c57163aebb4aaecb76
jin <jin@hyoo.ru>
2021-03-18 15:34:18 +0300

    Better styles

---
+++ app/app.view.css.ts
@@ -0,0 +1,27 @@
+namespace $.$$ {
+	
+	const { rem } = $mol_style_unit
+	
+	$mol_style_define( $hyoo_crowd_app, {
+		
+		$mol_page: {
+			
+			flex: {
+				grow: 1000,
+				shrink: 0,
+				basis: rem(20),
+			},
+						
+		},
+		
+		Left: {
+			margin: $mol_gap.block,
+		},
+		
+		Right: {
+			margin: $mol_gap.block,
+		},
+		
+	} )
+	
+}
