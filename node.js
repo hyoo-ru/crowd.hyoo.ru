af8a9ba80494ae48f03632b3cf3cc86691dc268f
nin-jin <nin-jin@users.noreply.github.com>
2021-12-06 07:02:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -131,7 +131,7 @@ var $;
             fetch('https://notify.bugsnag.com/', {
                 method: 'post',
                 body: JSON.stringify(report),
-            });
+            }).catch(() => null);
         }
     };
     globalThis.onunhandledrejection = function (event) {
