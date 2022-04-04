fe7baf0b8ba116e7d5ea54a8765078d0a8dab7b2
nin-jin <nin-jin@users.noreply.github.com>
2022-04-04 07:02:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -1488,7 +1488,12 @@ var $;
                 }
             }
             catch (error) {
-                result = error;
+                if (error instanceof Error || error instanceof Promise) {
+                    result = error;
+                }
+                else {
+                    result = new Error(String(error), { cause: error });
+                }
                 if (result instanceof Promise && !handled.has(result)) {
                     result = Object.assign(result.finally(() => {
                         if (this.cache === result)
@@ -1718,12 +1723,12 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    class $mol_wire_fiber_temp extends $mol_wire_fiber {
+    class $mol_wire_task extends $mol_wire_fiber {
         static getter(task) {
             return function $mol_wire_fiber_temp_get(host, args) {
                 const existen = $mol_wire_auto()?.track_next();
                 reuse: if (existen) {
-                    if (!(existen instanceof $mol_wire_fiber_temp))
+                    if (!(existen instanceof $mol_wire_task))
                         break reuse;
                     if (existen.host !== host)
                         break reuse;
@@ -1733,7 +1738,7 @@ var $;
                         break reuse;
                     return existen;
                 }
-                return new $mol_wire_fiber_temp(`${host?.[Symbol.toStringTag] ?? host}.${task.name}(#)`, task, host, ...args);
+                return new $mol_wire_task(`${host?.[Symbol.toStringTag] ?? host}.${task.name}(#)`, task, host, ...args);
             };
         }
         complete() {
@@ -1756,9 +1761,9 @@ var $;
             return next;
         }
     }
-    $.$mol_wire_fiber_temp = $mol_wire_fiber_temp;
+    $.$mol_wire_task = $mol_wire_task;
 })($ || ($ = {}));
-//mol/wire/fiber/temp/temp.ts
+//mol/wire/task/task.ts
 ;
 "use strict";
 var $;
@@ -1771,7 +1776,7 @@ var $;
         if (typeof sup[field] === 'function') {
             Object.defineProperty(orig, 'name', { value: sup[field].name });
         }
-        const temp = $mol_wire_fiber_temp.getter(orig);
+        const temp = $mol_wire_task.getter(orig);
         const value = function (...args) {
             const fiber = temp(this ?? null, args);
             return fiber.sync();
@@ -1789,7 +1794,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    class $mol_wire_fiber_persist extends $mol_wire_fiber {
+    class $mol_wire_atom extends $mol_wire_fiber {
         static getter(task, keys) {
             const field = task.name + '()';
             if (keys) {
@@ -1805,7 +1810,7 @@ var $;
                     else {
                         dict = (host ?? task)[field] = new Map();
                     }
-                    fiber = new $mol_wire_fiber_persist(key, task, host, ...args);
+                    fiber = new $mol_wire_atom(key, task, host, ...args);
                     dict.set(key, fiber);
                     return fiber;
                 };
@@ -1816,7 +1821,7 @@ var $;
                     if (existen)
                         return existen;
                     const key = `${host?.[Symbol.toStringTag] ?? host}.${field}`;
-                    const fiber = new $mol_wire_fiber_persist(key, task, host, ...args);
+                    const fiber = new $mol_wire_atom(key, task, host, ...args);
                     (host ?? task)[field] = fiber;
                     return fiber;
                 };
@@ -1880,13 +1885,13 @@ var $;
     }
     __decorate([
         $mol_wire_method
-    ], $mol_wire_fiber_persist.prototype, "recall", null);
+    ], $mol_wire_atom.prototype, "recall", null);
     __decorate([
         $mol_wire_method
-    ], $mol_wire_fiber_persist.prototype, "once", null);
-    $.$mol_wire_fiber_persist = $mol_wire_fiber_persist;
+    ], $mol_wire_atom.prototype, "once", null);
+    $.$mol_wire_atom = $mol_wire_atom;
 })($ || ($ = {}));
-//mol/wire/fiber/persist/persist.ts
+//mol/wire/atom/atom.ts
 ;
 "use strict";
 var $;
@@ -1912,11 +1917,13 @@ var $;
     $.$mol_wire_mem = $mol_wire_mem;
     function $mol_wire_mem_func(keys) {
         return (func) => {
-            const persist = $mol_wire_fiber_persist.getter(func, keys);
+            const persist = $mol_wire_atom.getter(func, keys);
             const wrapper = function (...args) {
                 let atom = persist(this, args.slice(0, keys));
                 if (args.length <= keys || args[keys] === undefined) {
-                    if ($mol_wire_auto() instanceof $mol_wire_fiber_temp) {
+                    if (!$mol_wire_fiber.warm)
+                        return atom.sync();
+                    if ($mol_wire_auto() instanceof $mol_wire_task) {
                         return atom.once();
                     }
                     else {
@@ -2116,7 +2123,7 @@ var $;
                 if (typeof val !== 'function')
                     return val;
                 let fiber;
-                const temp = $mol_wire_fiber_temp.getter(val);
+                const temp = $mol_wire_task.getter(val);
                 return function $mol_wire_async(...args) {
                     fiber?.destructor();
                     fiber = temp(obj, args);
@@ -9259,7 +9266,7 @@ var $;
                 const val = obj[field];
                 if (typeof val !== 'function')
                     return val;
-                const temp = $mol_wire_fiber_temp.getter(val);
+                const temp = $mol_wire_task.getter(val);
                 return function $mol_wire_sync(...args) {
                     const fiber = temp(obj, args);
                     return fiber.sync();
@@ -9836,6 +9843,32 @@ var $;
     });
 })($ || ($ = {}));
 //mol/wire/mem/mem.test.ts
+;
+"use strict";
+var $;
+(function ($) {
+    $mol_test({
+        'Previous value'() {
+            class Cache extends $mol_object2 {
+                static store(next) {
+                    if (!next)
+                        return {};
+                    return {
+                        ...$mol_wire_probe(() => this.store()) ?? {},
+                        ...next,
+                    };
+                }
+            }
+            __decorate([
+                $mol_wire_mem(0)
+            ], Cache, "store", null);
+            $mol_assert_like(Cache.store(), {});
+            $mol_assert_like(Cache.store({ foo: 666 }), { foo: 666 });
+            $mol_assert_like(Cache.store({ bar: 777 }), { foo: 666, bar: 777 });
+        },
+    });
+})($ || ($ = {}));
+//mol/wire/probe/probe.test.ts
 ;
 "use strict";
 var $;
