f7eef5a256ad94b76143a12e2285ffdfb8cd9c39
jin <jin@hyoo.ru>
2022-04-06 14:43:34 +0300

    Better view in demo

--- app/app.view.ts
+++ app/app.view.ts
@@ -38,19 +38,30 @@ namespace $.$$ {
 			return $hyoo_crowd_text.for( this.store() ).text( next )
 		}
 		
+		@ $mol_mem
 		delta() {
 			this.text()
 			return this.store().delta( this.sync_clock() )
 		}
 		
+		@ $mol_mem
+		delta_view() {
+			return this.delta().map( chunk => ({
+				head: chunk.head.toString(36).toUpperCase(),
+				self: chunk.self.toString(36).toUpperCase(),
+				prev: chunk.prev.toString(36).toUpperCase(),
+				next: chunk.next.toString(36).toUpperCase(),
+				peer: chunk.peer.toString(36).toUpperCase(),
+				time: chunk.time.toString(36).toUpperCase(),
+				data: JSON.stringify( chunk.data ),
+			}) )
+		}
+		
 		changes() {
-			this.text()
-			const clock = this.store().clock
-			return clock.now - this.sync_clock().now
+			return this.delta().length
 		}
 		
 		size_state() {
-			this.text()
 			return $mol_charset_encode( JSON.stringify( this.store() ) ).length
 		}
 		
@@ -76,19 +87,32 @@ namespace $.$$ {
 			return this.tokens_total() - this.tokens_alive()
 		}
 		
+		@ $mol_mem
+		size_state_bin() {
+			return this.store().delta().reduce( ( res, chunk )=> res + this.$.$hyoo_crowd_chunk_pack( chunk ).length, 0 )
+		}
+		
+		@ $mol_mem
+		size_delta_bin() {
+			return this.delta().reduce( ( res, chunk )=> res + this.$.$hyoo_crowd_chunk_pack( chunk ).length, 0 )
+		}
+		
 		stats() {
 			this.text()
 			return super.stats()
-			.replace( '{peer}', this.store().peer.toLocaleString() )
+			.replace( '{peer}', this.store().peer.toString(36).toUpperCase() )
 			.replace( '{changes}', this.changes().toLocaleString() )
 			.replace( '{tokens:alive}', this.tokens_alive().toLocaleString() )
 			.replace( '{tokens:dead}', this.tokens_dead().toLocaleString() )
 			.replace( '{tokens:total}', this.tokens_total().toLocaleString() )
-			.replace( '{stamp:now}', this.store().clock.now.toLocaleString() )
-			.replace( '{stamp:sync}', this.sync_clock().now.toLocaleString() )
+			.replace( '{stamp:now}', this.store().clock.now.toString(36).toUpperCase() )
+			.replace( '{stamp:sync}', this.sync_clock().now.toString(36).toUpperCase() )
 			.replace( '{size:text}', this.size_text().toLocaleString() )
 			.replace( '{size:state}', this.size_state().toLocaleString() )
 			.replace( '{size:delta}', this.size_delta().toLocaleString() )
+			.replace( '{size:text:bin}', this.size_text().toLocaleString() )
+			.replace( '{size:state:bin}', this.size_state_bin().toLocaleString() )
+			.replace( '{size:delta:bin}', this.size_delta_bin().toLocaleString() )
 		}
 		
 	}
