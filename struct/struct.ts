c03250f1f729e4b545e210ac5f52c4bb6333a194
jin <jin@hyoo.ru>
2022-09-03 12:41:27 +0300

    yoke shortcut for structs

--- struct/struct.ts
+++ struct/struct.ts
@@ -7,5 +7,16 @@ namespace $ {
 			return new Node( this.land, $mol_int62_hash_string( key + '\n' + this.head ) ) as InstanceType< Node >
 		}
 		
+		yoke< Node extends typeof $hyoo_crowd_node >(
+			key: string,
+			Node: Node,
+			king_level: $hyoo_crowd_peer_level,
+			base_level: $hyoo_crowd_peer_level,
+		) {
+			return this.sub( key, $hyoo_crowd_reg )
+				.yoke( king_level, base_level )!.chief
+				.sub( key, Node )
+		}
+		
 	}
 }
