f07984737849464ea0a92257daa390ffb2915087
nin-jin <nin-jin@users.noreply.github.com>
2022-09-25 07:08:28 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2328,6 +2328,7 @@ declare namespace $ {
         value(val?: any): string;
         hint(): string;
         enabled(): boolean;
+        spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
         Edit(): $mol_textarea_edit;
@@ -2597,10 +2598,18 @@ declare namespace $ {
             type: string;
         };
         sub(): readonly any[];
+        message(): {
+            hashchange: (next?: any) => any;
+        };
         mime(): string;
         title(): string;
         Fallback(): $$.$mol_link;
+        uri_change(next?: any): any;
     }
+}
+
+declare namespace $ {
+    function $mol_wire_solid(): void;
 }
 
 declare namespace $ {
@@ -2628,8 +2637,9 @@ declare namespace $.$$ {
         window(): Window;
         load(frame: HTMLIFrameElement): Promise<Window>;
         uri_resource(): string;
-        uri_listener(): $mol_dom_listener;
-        uri_change(event?: MessageEvent<[string, string]>): void;
+        message_listener(): $mol_dom_listener;
+        message_receive(event?: MessageEvent<[string, string]>): void;
+        uri_change(event: MessageEvent<[string, string]>): void;
         auto(): (Window | $mol_dom_listener)[];
     }
 }
