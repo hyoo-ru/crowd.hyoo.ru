6735ec1acf1f1049e530b3d72ab496e6036632c7
nin-jin <nin-jin@users.noreply.github.com>
2022-11-07 01:23:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -6125,6 +6125,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, auth.key_public_serial, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = true;
+            this.pub.emit();
         }
         leave() {
             const auth = this.peer();
@@ -6140,6 +6141,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, null, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = false;
+            this.pub.emit();
         }
         level_base(next) {
             this.level('0_0', next);
--- node.test.js
+++ node.test.js
@@ -6117,6 +6117,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, auth.key_public_serial, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = true;
+            this.pub.emit();
         }
         leave() {
             const auth = this.peer();
@@ -6132,6 +6133,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, null, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = false;
+            this.pub.emit();
         }
         level_base(next) {
             this.level('0_0', next);
