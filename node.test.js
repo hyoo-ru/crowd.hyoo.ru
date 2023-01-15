814860cd11348ce27ea9e5c14a38591042d693bd
nin-jin <nin-jin@users.noreply.github.com>
2023-01-15 07:01:51 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -1934,7 +1934,6 @@ var $;
                 if ($mol_owning_check(this, prev)) {
                     prev.destructor();
                 }
-                this.cache = next;
                 if ($mol_owning_catch(this, next)) {
                     try {
                         next[Symbol.toStringTag] = this[Symbol.toStringTag];
@@ -1946,6 +1945,7 @@ var $;
                 if (!this.sub_empty)
                     this.emit();
             }
+            this.cache = next;
             this.cursor = $mol_wire_cursor.fresh;
             if (next instanceof Promise)
                 return next;
