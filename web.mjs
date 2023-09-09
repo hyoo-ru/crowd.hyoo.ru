b5a1ac0363dcef2303da097278d00a4699e5c6d9
nin-jin <nin-jin@users.noreply.github.com>
2023-09-09 07:01:38 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -7789,6 +7789,11 @@ var $;
             Rows: {
                 padding: $mol_gap.text,
             },
+            Row: {
+                font: {
+                    family: 'inherit',
+                },
+            },
             Copy: {
                 alignSelf: 'flex-start',
                 justifySelf: 'flex-start',
