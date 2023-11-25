b25f4fc58e6af03503f120a37753be4b9b46a64f
nin-jin <nin-jin@users.noreply.github.com>
2023-11-25 11:33:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1619,10 +1619,6 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_base64_encode_safe(buffer: Uint8Array): string;
-}
-
-declare namespace $ {
     function $mol_base64_decode(base64: string): Uint8Array;
 }
 
@@ -1631,7 +1627,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_base64_decode_safe(str: string): Uint8Array;
+    function $mol_base64_url_encode(buffer: Uint8Array): string;
+    function $mol_base64_url_decode(str: string): Uint8Array;
 }
 
 declare namespace $ {
@@ -1792,8 +1789,8 @@ declare namespace $ {
 declare namespace $ {
     class $hyoo_crowd_fund<Node extends typeof $hyoo_crowd_node> extends $mol_object {
         world: $hyoo_crowd_world;
-        Node: Node;
-        constructor(world: $hyoo_crowd_world, Node: Node);
+        node_class: Node;
+        constructor(world: $hyoo_crowd_world, node_class: Node);
         Item(id: $mol_int62_string | `${$mol_int62_string}!${$mol_int62_string}`): InstanceType<Node>;
         make(law?: readonly ("" | `${string}_${string}`)[], mod?: readonly ("" | `${string}_${string}`)[], add?: readonly ("" | `${string}_${string}`)[]): InstanceType<Node>;
     }
@@ -2379,7 +2376,7 @@ declare namespace $ {
 declare namespace $.$$ {
     class $mol_hotkey extends $.$mol_hotkey {
         key(): {
-            [x: number]: ((event: KeyboardEvent) => void) | undefined;
+            readonly [x: number]: ((event: KeyboardEvent) => void) | undefined;
             readonly backspace?: ((event: KeyboardEvent) => void) | undefined;
             readonly tab?: ((event: KeyboardEvent) => void) | undefined;
             readonly enter?: ((event: KeyboardEvent) => void) | undefined;
