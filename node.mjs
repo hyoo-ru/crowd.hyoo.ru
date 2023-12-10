2b418c089b35707ca3249a8c97b0655b1fdea7bb
nin-jin <nin-jin@users.noreply.github.com>
2023-12-10 07:01:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -655,7 +655,7 @@ var $;
 var $;
 (function ($) {
     function $mol_promise_like(val) {
-        return val && typeof val.then === 'function';
+        return val && typeof val === 'object' && 'then' in val && typeof val.then === 'function';
     }
     $.$mol_promise_like = $mol_promise_like;
 })($ || ($ = {}));
@@ -923,6 +923,8 @@ var $;
                 return value;
             if (value instanceof RegExp)
                 return value.toString();
+            if (value instanceof Uint8Array)
+                return [...value];
             let key = $.$mol_key_store.get(value);
             if (key)
                 return key;
